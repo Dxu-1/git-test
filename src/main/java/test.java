0662eb58d5e720f4b7b366e002d60f38c7104ecf
Dxu-1 <89635048+Dxu-1@users.noreply.github.com>
2021-12-31 22:00:00 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -7,5 +7,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello push");
+        System.out.println("hello pull");
     }
 }
